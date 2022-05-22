4bad1f1cb9e220e0122fa5a971e4a9189e7555de
HenessThoven <106001893+HenessThoven@users.noreply.github.com>
2022-05-22 10:27:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+	titleWeb: "Anh xin lỗi em ạ :(",
+	introTitle: 'Bảo Ngọc',
+	introDesc:,
+	btnIntro: 'Ấn vô đây đi em :(',
+	title: 'Em à, anh vẫn còn yêu em nhiều lắm ạ:(, hứa với anh là đừng vì những lỗi lầm nhỏ nhặt mà chia tay nha. Anh vẫn còn yêu em lắm🥰',
+	desc: 'Đừng giận nữa mà ',
+	btnYes: 'Dạ <3',
+	btnNo: 'Có cl t hết dỗi',
+	question: 'Trên thế giới hơn 7 tỉ người mà anh chỉ yêu mình em thôi <3',
+	btnReply: 'Dạ',
+	reply: 'Oke anh ạ:3, cảm ơn anh vì trang web, chúng ta yêu tiếp nha em vẫn yêu anh :33',
+	mess: 'Anh biết em vẫn thương anh mà 🥰. Yêu em nhiều nhiều 😘😘',
+	messDesc: 'Vậy không dỗi nữa nha :3 yêu em nhiều',
+	btnAccept: 'Dạ <3',
+	messLink: 'https://www.facebook.com/messages/t/100054884670085 '
+	}
